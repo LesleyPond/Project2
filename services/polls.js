472c37e6f0b8d3afb99149928d297c7c57351742
johnale <johnale@cisco.com>
2019-04-01 21:07:58 -0400

    updated the update api to use the sessionID for the vote.

--- services/polls.js
+++ services/polls.js
@@ -6,38 +6,41 @@ const getAll = id => db.Poll.findAll({where: {UserId: id}});
 
 const getById = sessionID => db.Poll.findOne({where: {sessionID: sessionID}});
 
-const updatePoll = (id, voteCast) => {
+const updatePoll = (sessionID, voteCast) => {
     switch (voteCast) {
      case 'option1Votes':
-        db.Poll.increment('option1Votes', { where: { UserId: id }});
+        db.Poll.increment('option1Votes', { where: { sessionID: sessionID }});
         break;
      case 'option2Votes' :
-        db.Poll.increment('option2Votes', { where: { UserId: id }});
+        db.Poll.increment('option2Votes', { where: { sessionID: sessionID }});
         break;
      case 'option3Votes' :
-        db.Poll.increment('option3Votes', { where: { UserId: id }});
+        db.Poll.increment('option3Votes', { where: { sessionID: sessionID }});
         break;
      case 'option4Votes' :
-        db.Poll.increment('option4Votes', { where: { UserId: id }});
+        db.Poll.increment('option4Votes', { where: { sessionID: sessionID }});
         break;
      case 'option5Votes' :
-        db.Poll.increment('option5Votes', { where: { UserId: id }});
+        db.Poll.increment('option5Votes', { where: { sessionID: sessionID }});
         break;
      case 'option6Votes' :
-        db.Poll.increment('option6Votes', { where: { UserId: id }});
+        db.Poll.increment('option6Votes', { where: { sessionID: sessionID }});
         break;
      case 'option7Votes' :
-        db.Poll.increment('option7Votes', { where: { UserId: id }});
+        db.Poll.increment('option7Votes', { where: { sessionID: sessionID }});
         break;
      case 'option8Votes' :
-        db.Poll.increment('option8Votes', { where: { UserId: id }});
+        db.Poll.increment('option8Votes', { where: { sessionID: sessionID }});
         break;
      case 'option9Votes' :
-        db.Poll.increment('option9Votes', { where: { UserId: id }});
+        db.Poll.increment('option9Votes', { where: { sessionID: sessionID }});
         break;
      case 'option10Votes' :
-        db.Poll.increment('option10Votes', { where: { UserId: id }});
+        db.Poll.increment('option10Votes', { where: { sessionID: sessionID }});
         break;
+      default:
+         console.log('not a valid option');
+         break;
     }
 
 
