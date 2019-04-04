59d53b66c12314f75651937677aa88ca3152b03d
Janxxy <lesley.pond@gmail.com>
2019-04-04 17:25:24 -0400

    mobile responsive

--- services/user.js
+++ services/user.js
@@ -1,10 +1,12 @@
 const db = require('../models');
 
-const getUserByEmail = (email) => db.User.findOne({where: {email_address: email}})
+const getUserByEmail = (email) => db.User.findOne({
+  where: {email_address: email}});
 
 const createUser = (user) => db.User.create(user);
 
-const changePassword = (id, password) => db.User.update({password: password}, {where: {id: id}});
+const changePassword = (id, password) => db.User.update({
+  password: password}, {where: {id: id}});
 
 module.exports = {
   getUserByEmail: getUserByEmail,
